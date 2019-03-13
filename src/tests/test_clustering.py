804655f90252ea0f07923fd259a482b6a80017b4
David Heller <heller_d@molgen.mpg.de>
2019-03-13 11:27:13 +0100

    add tests for clustering and combining components

---
+++ src/tests/test_clustering.py
@@ -0,0 +1,42 @@
+import unittest
+
+from random import uniform
+
+from svim.SVIM_clustering import partition_and_cluster
+from svim.SVSignature import SignatureDeletion
+from svim.SVIM_input_parsing import parse_arguments
+
+class TestSVIMClustering(unittest.TestCase):
+    def setUp(self):
+        self.signatures = []
+        for i in range(10):
+            center = 100000 + uniform(-100, 100)
+            half_span = 1000 + uniform(-100, 100)
+            new_sig = SignatureDeletion("chr1", center - half_span, center + half_span, "cigar", str(i))
+            self.signatures.append(new_sig)
+        for i in range(10, 20):
+            center = 200000 + uniform(-100, 100)
+            half_span = 1000 + uniform(-100, 100)
+            new_sig = SignatureDeletion("chr1", center - half_span, center + half_span, "cigar", str(i))
+            self.signatures.append(new_sig)
+        for i in range(20, 30):
+            center = 100000 + uniform(-100, 100)
+            half_span = 2000 + uniform(-100, 100)
+            new_sig = SignatureDeletion("chr1", center - half_span, center + half_span, "cigar", str(i))
+            self.signatures.append(new_sig)
+        self.options = parse_arguments('0.5.0', ['alignment', 'myworkdir', 'mybamfile'])
+
+    def test_clustering(self):
+        clusters = partition_and_cluster(self.signatures, options=self.options, type="deleted regions")
+        self.assertEqual(len(clusters), 3)
+        for cluster in clusters:
+            self.assertEqual(len(set([int(member.read) // 10 for member in cluster.members])), 1)
+    
+    def test_scores(self):
+        clusters = partition_and_cluster(self.signatures, options=self.options, type="deleted regions")
+        for cluster in clusters:
+            self.assertGreaterEqual(cluster.score, 10)
+            self.assertLessEqual(cluster.score, 10 + 20/8)
+
+if __name__ == '__main__':
+    unittest.main()
